3710065a5da8634c4bcdb521814c86d94343d10b
Jared Roth <jared.roth@gmail.com>
2016-03-12 14:53:39 -0800

    I4.3: All extensions completed

--- web_guesser.rb
+++ web_guesser.rb
@@ -18,23 +18,26 @@ end
 def check_guess(guess)
   if guess - @@number > 5
     ["Way too high!", "#FF5F3B"]
-  elsif @@number < guess
+  elsif guess > @@number
     ["Too High!", "#F7AB99"]
   elsif @@number - guess > 5
     ["Way too low!", "#FF5F3B"]
-  elsif guess < @@number
+  elsif @@number > guess
     ["Too Low!", "#F7AB99"]
   else
     @@number = rand(100)
     @@guesses = 5
-    ["<p>You got it right!</p>
-     <p>The SECRET NUMBER is #{@@number}</p>", "#b0e0e6"]
+    ["You got it right!
+     <p>The SECRET NUMBER is #{guess}</p>", "#b0e0e6"]
   end
 end
 get '/' do
   guess = params['guess'].to_i
+  if params['cheat']
+    cheat = "<p>The SECRET NUMBER is #{@@number}</p>"
+  end
   result = make_guess(guess)
   message = result[0]
   hex = result[1]
-  erb :index, :locals => {:message => message, :hex => hex}
+  erb :index, :locals => {:message => message, :hex => hex, :cheat => cheat}
 end
