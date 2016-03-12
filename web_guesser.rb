a2e5ee6d0c93a2ae718c1a3da79a60773e573c0d
Jared Roth <jared.roth@gmail.com>
2016-03-12 14:18:38 -0800

    I4.1: Color change dependent on guess range

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,20 +4,22 @@ require 'sinatra/reloader'
 SECRET_NUMBER = rand(100)
 def check_guess(guess)
   if guess - SECRET_NUMBER > 5
-    "Way too high!"
+    ["Way too high!", "#FF5F3B"]
   elsif SECRET_NUMBER < guess
-    "Too High!"
+    ["Too High!", "#F7AB99"]
   elsif SECRET_NUMBER - guess > 5
-    "Way too low!"
+    ["Way too low!", "#FF5F3B"]
   elsif guess < SECRET_NUMBER
-    "Too Low!"
+    ["Too Low!", "#F7AB99"]
   else
-    "<p>You got it right!</p>
-     <p>The SECRET NUMBER is #{SECRET_NUMBER}</p>"
+    ["<p>You got it right!</p>
+     <p>The SECRET NUMBER is #{SECRET_NUMBER}</p>", "#b0e0e6"]
   end
 end
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:message => message}
+  result = check_guess(guess)
+  message = result[0]
+  hex = result[1]
+  erb :index, :locals => {:message => message, :hex => hex}
 end
