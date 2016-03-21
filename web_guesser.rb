d9d3b2db842a44d1ff7006611ca7d2fec9eecdb4
Jared Roth <jared.roth@gmail.com>
2016-03-21 11:02:25 -0600

    Relocated background colors into variables

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,15 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-@@number = rand(100)
+@@number  = rand(100)
 @@guesses = 5
+@@close   = "#F7AB99"
+@@far     = "#FF5F3B"
+@@correct = "#B0E0E6"
 
 def make_guess(guess)
   @@guesses -= 1
   if @@guesses < 0
     @@number = rand(100)
     @@guesses = 5
-    ["You have lost, a new number has been generated", "#FF5F3B"]
+    ["You have lost, a new number has been generated", @@far]
   else
     check_guess(guess)
   end
@@ -17,18 +20,18 @@ end
 
 def check_guess(guess)
   if guess - @@number > 5
-    ["Way too high!", "#FF5F3B"]
+    ["Way too high!", @@far]
   elsif guess > @@number
-    ["Too High!", "#F7AB99"]
+    ["Too High!", @@close]
   elsif @@number - guess > 5
-    ["Way too low!", "#FF5F3B"]
+    ["Way too low!", @@far]
   elsif @@number > guess
-    ["Too Low!", "#F7AB99"]
+    ["Too Low!", @@close]
   else
     @@number = rand(100)
     @@guesses = 5
     ["You got it right!</p>
-<p>The SECRET NUMBER is #{guess}", "#b0e0e6"]
+<p>The SECRET NUMBER is #{guess}", @@correct]
   end
 end
 
